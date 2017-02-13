55556de2a0932fee36cb30edc3c292174a46df6a
ilham farobi <ilhamfarobi@gmail.com>
2017-02-13 20:47:25 +0700

    add react bootstrap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import path from 'path';
 
 import {Router, browserHistory, hashHistory} from 'react-router';
 
 import App from './App';
 
 let history = (Modernizr && Modernizr.history) ? browserHistory : hashHistory;
+
+global.SRCDIR = path.resolve(__dirname);
 
 const rootRoute = {
   childRoutes: [{
